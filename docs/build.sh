6af1d4398d5e5490367936ddb3d8d9939ffc0430
Eric Schrock <Eric.Schrock@delphix.com>
2019-09-24 08:53:51 -0400

    add release type and version

--- docs/build.sh
+++ docs/build.sh
@@ -7,16 +7,39 @@ VENV_DIR=$BUILD_DIR/venv
 OUT_DIR=$BUILD_DIR/out
 SRC_DIR=$(dirname $0)/src
 
+function usage() {
+  echo "Usage: $0 [-r development|official] [-v version]" 1>&2
+  exit 2
+}
+
+release_type=development
+version=latest
+
+while getopts ":r:v:" o; do
+  case "${o}" in
+    r)
+      release_type=$OPTARG
+      ;;
+    v)
+      version=$OPTARG
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+
 mkdir -p $OUT_DIR
 
 #
 # Create python environment
 #
 if [[ -z $VIRTUAL_ENV ]]; then
-    if [[ ! -d $VENV_DIR ]]; then
-      virtualenv $VENV_DIR --no-site-packages --python=python3
-    fi
-    source $VENV_DIR/bin/activate
+  if [[ ! -d $VENV_DIR ]]; then
+    virtualenv $VENV_DIR --no-site-packages --python=python3
+  fi
+  source $VENV_DIR/bin/activate
 fi
 
 #
@@ -27,4 +50,5 @@ pip3 install -r $(dirname $0)/requirements.txt
 #
 # Run sphinx
 #
-sphinx-build -W --keep-going $SRC_DIR $OUT_DIR
+sphinx-build -W --keep-going $SRC_DIR $OUT_DIR -D release_type=$release_type \
+  -D version=$version
