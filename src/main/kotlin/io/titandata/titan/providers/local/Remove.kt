c215dbf9952d12de6b4e57c63aa279d8330bd7d4
Derek Smart <derek.smart@delphix.com>
2019-09-25 19:02:25 -0400

    note for volumeApi fallback
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/kotlin/io/titandata/titan/providers/local/Remove.kt
+++ src/main/kotlin/io/titandata/titan/providers/local/Remove.kt
@@ -47,6 +47,13 @@ class Remove (
                 try {
                     docker.removeVolume(volume.name, force)
                 } catch (e: CommandException) {
+                    /**
+                     Docker will sometimes fail to launch a container after the
+                     volume has been created. The container does not exist, but
+                     docker thinks the volume is attached to a container and does
+                     not allow it to be removed. Falling back on the VolumeApi
+                     fixes this condition.
+                     */
                     volumeApi.removeVolume(volRequest)
                 }
             }
