fdd66acf922f0091d2cb458023c847eaf9420416
Derek Smart <derek.smart@delphix.com>
2019-10-09 17:07:26 -0400

    remove hard coded 0.3.2
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- scripts/windows/build-native.cmd
+++ scripts/windows/build-native.cmd
@@ -2,3 +2,4 @@
 set /p Version=<VERSION
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release
 native-image -cp target\titan-%Version%-jar-with-dependencies.jar -H:Name=titan -H:Class=io.titandata.titan.Cli -H:+ReportUnsupportedElementsAtRuntime -H:ReflectionConfigurationFiles=config\reflect-config.json -H:ResourceConfigurationFiles=config\resource-config.json -H:JNIConfigurationFiles=config\jni-config.json -H:+AddAllCharsets --allow-incomplete-classpath --enable-http
+Compress-Archive -Path titan.exe -CompressionLevel Optimal -DestinationPath titan-%Version%-windows_amd64.zip
