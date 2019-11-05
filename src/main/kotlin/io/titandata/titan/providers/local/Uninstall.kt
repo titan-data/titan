61e91ae41337f9b26f8dfd3b75971f7d9ebb8800
Derek Smart <derek.smart@delphix.com>
2019-11-05 17:41:11 -0500

    Fixes #26
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/kotlin/io/titandata/titan/providers/local/Uninstall.kt
+++ src/main/kotlin/io/titandata/titan/providers/local/Uninstall.kt
@@ -25,7 +25,7 @@ class Uninstall (
                 if (!force) {
                     exit("repository ${repo.name} exists, remove first or use '-f'", 1)
                 }
-                remove(repo.name, true)
+                track("Removing repository ${repo.name}" ) { remove(repo.name, true) }
             }
         }
         if (docker.titanServerIsAvailable()) docker.rm("titan-server", true)
