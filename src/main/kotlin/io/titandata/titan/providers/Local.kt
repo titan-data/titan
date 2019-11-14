26b1128ac7a217d1ec74448ede42fe81bd7d5b2c
Derek Smart <derek.smart@delphix.com>
2019-11-14 13:17:28 -0500

    bump to 0.6.1
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/kotlin/io/titandata/titan/providers/Local.kt
+++ src/main/kotlin/io/titandata/titan/providers/Local.kt
@@ -18,7 +18,7 @@ data class Container (
 )
 
 class Local: Provider {
-    private val titanServerVersion = "0.6.0"
+    private val titanServerVersion = "0.6.1"
     private val dockerRegistryUrl = "titandata"
 
     private val httpHandler = HttpHandler()
