296144ee256cf0db7df588fb6c8d5fd36a024448
Eric Schrock <Eric.Schrock@delphix.com>
2019-11-25 10:41:26 -0500

    lint

--- src/main/kotlin/io/titandata/titan/providers/ProviderFactory.kt
+++ src/main/kotlin/io/titandata/titan/providers/ProviderFactory.kt
@@ -2,22 +2,22 @@ package io.titandata.titan.providers
 
 class ProviderFactory {
 
-    private val provider : Provider
+    private val provider: Provider
 
     init {
         val type = System.getenv("TITAN_CONTEXT") ?: "local"
-        provider = when(type) {
+        provider = when (type) {
             "local" -> Local()
             "kubernetes" -> Kubernetes()
             else -> Mock()
         }
     }
 
-    fun list() : List<Provider> {
+    fun list(): List<Provider> {
         return listOf(provider)
     }
 
-    fun byRepositoryName(repoName: String?) : Provider {
+    fun byRepositoryName(repoName: String?): Provider {
         if (repoName == null) {
             return default()
         } else {
@@ -25,11 +25,11 @@ class ProviderFactory {
         }
     }
 
-    fun byRepository(repoName: String) : Provider {
+    fun byRepository(repoName: String): Provider {
         return provider
     }
 
-    fun default(checkInstall : Boolean = true) : Provider {
+    fun default(checkInstall: Boolean = true): Provider {
         if (checkInstall) {
             provider.checkInstall()
         }
