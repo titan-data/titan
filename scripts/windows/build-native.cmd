77781388d80e3233fe7c27f053126b51b575a28d
Derek Smart <derek@grindaga.com>
2020-10-15 10:28:59 -0400

    remove kotlin code and references
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- scripts/windows/build-native.cmd
+++
@@ -1,4 +0,0 @@
-@echo on
-set /p Version=<VERSION
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release
-native-image -cp target\titan-%Version%.jar -H:Name=titan -H:Class=io.titandata.titan.Cli -H:+ReportUnsupportedElementsAtRuntime -H:ReflectionConfigurationFiles=config\reflect-config.json -H:ResourceConfigurationFiles=config\resource-config.json -H:JNIConfigurationFiles=config\jni-config.json -H:+AddAllCharsets --allow-incomplete-classpath --enable-http
--- src/endtoend-test/context/docker/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/bin/bash -c printenv > /usr/share/nginx/html/index.html
-nginx -g "daemon off;"
